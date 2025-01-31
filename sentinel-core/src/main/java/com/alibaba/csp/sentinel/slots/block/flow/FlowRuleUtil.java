99309d2c4e420c48b5c4d0f7369e4c7a82ba8139
lingma-ai-developer <lingma@service.aliyun.com>
2025-01-31 22:05:06 +0000

    fix #1

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/FlowRuleUtil.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/FlowRuleUtil.java
@@ -198,6 +198,8 @@ public final class FlowRuleUtil {
         switch (rule.getStrategy()) {
             case ClusterRuleConstant.FLOW_CLUSTER_STRATEGY_NORMAL:
                 return true;
+            case ClusterRuleConstant.FLOW_CLUSTER_STRATEGY_FALLBACK_TO_LOCAL:
+                return clusterConfig.isFallbackToLocalWhenFail();
             default:
                 return false;
         }
